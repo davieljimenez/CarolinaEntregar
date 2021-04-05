8f21f72616abba14c3a79764d9450d11244417b8
Daviel Antonio Jiménez Comas <49133701+davieljimenez@users.noreply.github.com>
2021-04-04 22:51:31 -0400

    Entregando

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -1,23 +1,9 @@
-// module.exports = {
-//     HOST: "localhost",
-//     USER: "root",
-//     PASSWORD: "toor",
-//     DB: "carolina_spa_db",
-//     dialect: "mysql",
-//     pool: {
-//         max: 5,
-//         min: 0,
-//         acquire: 30000,
-//         idle: 10000,
-//     },
-// };
-
 module.exports = {
-    HOST: "queenie.db.elephantsql.com",
-    USER: "knbknhlm",
-    PASSWORD: "wVokDA8GRR3YExyd1kYBkKLMMX5cXIKg",
-    DB: "knbknhlm",
-    dialect: "postgres",
+    HOST: "localhost",
+    USER: "root",
+    PASSWORD: "toor",
+    DB: "carolina_spa_db",
+    dialect: "mysql",
     pool: {
         max: 5,
         min: 0,
@@ -25,3 +11,19 @@ module.exports = {
         idle: 10000,
     },
 };
+
+
+// module.exports = {
+//     HOST: "queenie.db.elephantsql.com",
+//     USER: "ktiieqzh",
+//     PASSWORD: "Nr54tzq3H6GWnKrvf-XKQFe2EoX9FyyJ",
+//     DB: "ktiieqzh",
+//     dialect: "postgres",
+//     pool: {
+//         max: 5,
+//         min: 0,
+//         acquire: 30000,
+//         idle: 10000,
+//     },
+
+// }
