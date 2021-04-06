18e740e0e256348b14949f70fa953dd2a9adaade
mulloa <mulloa@LTP-PCN-05>
2021-04-05 22:26:50 -0400

    Added logo and changed navbar label to CarolinaSPA

--- app/src/pages/SignIn.jsx
+++ app/src/pages/SignIn.jsx
@@ -5,6 +5,7 @@ import axios from "axios";
 import swal from "sweetalert";
 import { TOKEN_NAME } from "../config/constants";
 import { setAuthorizationToken } from "../utils/api";
+import logo from '../logo.jpeg';
 
 // All this shit should be refactored in reusable components, I know it but I just don't give a fuck
 // because this is not my project
@@ -36,36 +37,43 @@ const SignIn = ({ history }) => {
   return (
     <div className="container">
       <div className="card bg-light">
-        <article className="card-body mx-auto" style={{ maxWidth: "400px" }}>
-          <h4 className="card-title mt-3 text-center">Inicia Sesión</h4>
-          <form onSubmit={handleSubmit}>
-            <TextField
-              icon="fa fa-envelope"
-              name="Email"
-              placeholder="Email"
-              type="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-            <TextField
-              icon="fa fa-lock"
-              name="Contrasena"
-              placeholder="Contraseña"
-              type="password"
-              value={pass}
-              onChange={(e) => setPass(e.target.value)}
-            />
-            <div className="form-group">
-              <button type="submit" className="btn btn-primary btn-block">
-                {" "}
-                Inicia Sesión{" "}
-              </button>
-            </div>
-            <Link className="text-center" to="/signup">
-              No tienes una cuenta – Créala aquí
-            </Link>{" "}
-          </form>
-        </article>
+        <div className="row">
+          <div className="col-5">
+            <img className="ml-5 mt-5 shadow-lg border rounded float-right" src={logo} alt=""/>
+          </div>
+          <div className="col-6">
+            <article className="card-body mx-auto" style={{ maxWidth: "400px" }}>
+                <h4 className="card-title mt-3 text-center">Inicia Sesión</h4>
+              <form onSubmit={handleSubmit}>
+                <TextField
+                  icon="fa fa-envelope"
+                  name="Email"
+                  placeholder="Email"
+                  type="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+                <TextField
+                  icon="fa fa-lock"
+                  name="Contrasena"
+                  placeholder="Contraseña"
+                  type="password"
+                  value={pass}
+                  onChange={(e) => setPass(e.target.value)}
+                />
+                <div className="form-group">
+                  <button type="submit" className="btn btn-primary btn-block">
+                    {" "}
+                    Inicia Sesión{" "}
+                  </button>
+                </div>
+                <Link className="text-center" to="/signup">
+                  No tienes una cuenta – Créala aquí
+                </Link>{" "}
+              </form>
+            </article>
+          </div>
+        </div>
       </div>
     </div>
   );
